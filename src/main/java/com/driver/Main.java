2a44dc8a38b335e639178ff7c205ef3559f20878
Saqlaen <msaqlaenahmed98@gmail.com>
2022-12-12 10:21:42 +0530

    wrote getters and setters

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args ){
+      RWOnly obj = new RWOnly();
+//      obj.name = "abc";
+//      System.out.println( obj.name );
+      obj.setters("abc");
+      System.out.println( obj.getter() );
+  }
 }
